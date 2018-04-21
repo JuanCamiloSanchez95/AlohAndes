36e00d47f1c59c4f94c0d424a4be1ebb755828c3
LAPTOP-C6DCBCLI\cmama <cm.amaya@hotmail.com>
2018-04-21 07:42:55 -0500

    Se agrega los archivos sql al proyecto

---
+++ docs/CREACION TABLAS/AgregarDatosGRADO2.sql
@@ -0,0 +1,18 @@
+insert into alojamientosApartamentos(id,amoblado,administracion) values (7,'Y',322173871);
+insert into HabitacionesH(id,capacidad,tipo,numeroHabitacion,ubicacionH,tamano) values(6,3,'Suite',124,'Torre al lado de la piscina','Grande');
+insert into HabitacionesUniversitarias(id, compartida, numeroHabitacion,capacidad,menaje,ubicacionH) values (2,'Y', 312,3,'wddasda','dasdadas');
+insert into HabitacionesUniversitarias(id, compartida, numeroHabitacion,capacidad,menaje,ubicacionH) values (4,'N', 432,1,'dadsad','dasdwr');
+insert into habitacionesVivienda(id,compartida,urlEsquema) values(1,'Y','WEQEWQEQ');
+insert into alojamientosoperadores(operadorid, alojamientoid) values (5,1);
+insert into alojamientosoperadores(operadorid, alojamientoid) values (5,2);
+insert into alojamientosoperadores(operadorid, alojamientoid) values (6,3);
+insert into alojamientosoperadores(operadorid, alojamientoid) values (8,4);
+insert into alojamientosoperadores(operadorid, alojamientoid) values (5,5);
+insert into alojamientosoperadores(operadorid, alojamientoid) values (1,6);
+insert into alojamientosoperadores(operadorid, alojamientoid) values (6,7);
+insert into seguros(id,nombre,costo) values (1,'ksadajdka', 31231.31);
+insert into seguros(id,nombre,costo) values (2,'weqeqweqe', 321312.312);
+insert into seguros(id,nombre,costo) values (3,'dipsaudpi', 318923812.321);
+insert into alojamientosviviendas(id,numeroHabitaciones,menaje,idS) values (3,2,'dsaakj',1);
+insert into alojamientosviviendas(id,numeroHabitaciones,menaje,idS) values (5,3,'dsaakj',2);
+
