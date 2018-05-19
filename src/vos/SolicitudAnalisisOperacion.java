bcad951142a2927d321a2aa6a623ae4ff9038d2d
LAPTOP-C6DCBCLI\cmama <cm.amaya@hotmail.com>
2018-05-19 18:41:01 -0500

    Avance RFC7

---
+++ src/vos/SolicitudAnalisisOperacion.java
@@ -0,0 +1,65 @@
+package vos;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+/**
+ * Clase que representa una solicitud de analisis de operacion.
+ * @author Cristian 
+ */
+public class SolicitudAnalisisOperacion {
+	
+	// ----------------------------------------------------------------------------------------------------------------------------------
+	// ATRIBUTOS
+	// ----------------------------------------------------------------------------------------------------------------------------------
+
+	@JsonProperty(value = "categoria")
+	private String categoria;
+	
+	@JsonProperty(value = "unidadTiempo")
+	private String unidadTiempo;
+	
+	// ----------------------------------------------------------------------------------------------------------------------------------
+	// METODO CONSTRUCTOR
+	// ----------------------------------------------------------------------------------------------------------------------------------
+
+
+	/**
+	 * Constructor de solicitud de analisis de operacion.
+	 * @param categoria - categoria de Alojamiento
+	 * @param unidadTiempo - Unitdad de Tiempo (Dia Mes,Semana,Año)
+	 */
+	public  SolicitudAnalisisOperacion(@JsonProperty(value = "categoria") String categoria,@JsonProperty(value = "unidadTiempo") String unidadTiempo) {
+		this.categoria = categoria;
+		this.unidadTiempo = unidadTiempo;
+	}
+	
+	// ----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS DE LA CLASE
+	// ----------------------------------------------------------------------------------------------------------------------------------
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public String getUnidadTiempo() {
+		return unidadTiempo;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public void setUnidadTiempo(String unidadTiempo) {
+		this.unidadTiempo = unidadTiempo;
+	}
+	
+	public boolean validUnidadDeTiempo() {
+		return this.unidadTiempo.equalsIgnoreCase("DIA") || this.unidadTiempo.equalsIgnoreCase("SEMANA")|| this.unidadTiempo.equalsIgnoreCase("MES") ||this.unidadTiempo.equalsIgnoreCase("AÑO");
+	}
+	
+	public boolean validCategoria() {
+		return this.categoria.equalsIgnoreCase("Apartamento")||this.categoria.equalsIgnoreCase("HabitacionHotel")||this.categoria.equalsIgnoreCase("HabitacionHostal")||this.categoria.equalsIgnoreCase("Vivienda")
+				||this.categoria.equalsIgnoreCase("HabitacionVivienda")||this.categoria.equalsIgnoreCase("HabitacionUniversitaria");
+	}
+	
+}
